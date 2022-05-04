98422c21d9781e9a15c66c5b8a4cbd2a009636f0
Jingyu Lee <dostos10@gmail.com>
2022-05-04 14:00:48 +0900

    Nullity check for TensorsFromTArray

--- Band/Source/Band/Private/BandTensorUtil.cpp
+++ Band/Source/Band/Private/BandTensorUtil.cpp
@@ -3,10 +3,17 @@
 TArray<TfLiteTensor*> BandTensorUtil::TensorsFromTArray(TArray<UBandTensor*> Tensors)
 {
 	TArray<TfLiteTensor*> OutTensors;
-
+	
 	for (int i = 0; i < Tensors.Num(); i++)
 	{
-		OutTensors.Push(Tensors[i]->Handle());
+		if (Tensors[i])
+		{
+			OutTensors.Push(Tensors[i]->Handle());
+		}
+		else
+		{
+			UE_LOG(LogBand, Error, TEXT("Try to access null tensor."));
+		}
 	}
 
 	return OutTensors;
