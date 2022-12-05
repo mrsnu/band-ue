cac70615525441838c63a3c22369341b91b2ab9a
Jingyu Lee <dostos10@gmail.com>
2022-12-05 16:16:08 +0900

    Conditional branch wip

--- Band/Source/Band/Public/Rect.h
+++ Band/Source/Band/Public/Rect.h
@@ -3,8 +3,7 @@
 #include "Rect.generated.h"
 
 USTRUCT(BlueprintType)
-struct FRect
-{
+struct FRect {
 	GENERATED_BODY()
 
 	float Left;
@@ -13,8 +12,9 @@ struct FRect
 	float Bottom;
 };
 
-inline float BoxIntersection(FRect A, FRect B)
-{
+inline float BoxArea(FRect A) { return (A.Right - A.Left) * (A.Top - A.Bottom); }
+
+inline float BoxIntersection(FRect A, FRect B) {
 	if (A.Right <= B.Left || B.Right <= A.Left)
 		return 0.0f;
 	if (A.Top <= B.Bottom || B.Top <= A.Bottom)
@@ -24,14 +24,6 @@ inline float BoxIntersection(FRect A, FRect B)
 }
 
 
-inline float BoxUnion(FRect A, FRect B)
-{
-	return (A.Right - A.Left) * (A.Top - A.Bottom)
-		+ (B.Right - B.Left) * (B.Top - B.Bottom)
-		- BoxIntersection(A, B);
-}
+inline float BoxUnion(FRect A, FRect B) { return BoxArea(A) + BoxArea(B) - BoxIntersection(A, B); }
 
-inline float BoxIou(FRect A, FRect B)
-{
-	return BoxIntersection(A, B) / BoxUnion(A, B);
-}
+inline float BoxIou(FRect A, FRect B) { return BoxIntersection(A, B) / BoxUnion(A, B); }
